8a3c52b8e39ef8be87db43ba7d62c189b5f18150
Giuxpro <albertogiusepp@gmail.com>
2022-11-10 19:09:45 -0300

    adding comments to main file

--- client/js/main.js
+++ client/js/main.js
@@ -10,6 +10,11 @@ const categories = document.querySelector('#categories');
 
 window.addEventListener('DOMContentLoaded', appInit);
 
+/**
+ * The appInit function is called when the page loads, and it calls the getCategories and getProducts
+ * functions.
+ * @returns Nothing.
+ */
 function appInit() {
     getCategories();
     getProducts();
@@ -18,6 +23,8 @@ function appInit() {
 
 let searchForProduct = '';
 
+/* Listening for a click event on the categories element. If the value of the target is 0 it will call
+the getProducts function, otherwise it will call the getProductsByCategories function. */
 categories.addEventListener('click', (e) => {
     if(e.target.value == 0){
         getProducts();
@@ -27,11 +34,18 @@ categories.addEventListener('click', (e) => {
 });
 
 
+/* Listening for an input event on the inputSearch element to asign
+* the value to searchForProduct. 
+*/
 inputSearch.addEventListener('input', (e) => {
     e.preventDefault();
     searchForProduct = e.target.value;
 });
 
+/* An event listener that is listening for a click event on the button. 
+*  if searchForProduct is empty will return an error,
+*  otherwise it will call getProductsByName function 
+*/
 btnSubmit.addEventListener('click', (e) => {
     e.preventDefault();
     if(searchForProduct === ''){
