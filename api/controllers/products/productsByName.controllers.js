a83d15d3cf5d68570b48d53ce7ce53de8c53924a
Giuxpro <albertogiusepp@gmail.com>
2022-11-10 17:16:15 -0300

    some text traduction for msjError returned to the database

--- api/controllers/products/productsByName.controllers.js
+++ api/controllers/products/productsByName.controllers.js
@@ -5,7 +5,7 @@ const getProductsByName = async (res, name) => {
     try {
         await db.query(`SELECT * FROM product WHERE name LIKE '%${name}%'`, (err, rows, fields) => {
             if(rows.length === 0) {
-                return res.status(404).json({error:`There are no product with name '%${name}`});
+                return res.status(404).json({error:`Lo sentimos de momento no tenemos el producto con el nombre '%${name}`});
             } else {
                 res.send(rows);
             }
